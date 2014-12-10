0792533e06768ad76da9d1c2d2bf9f6ea1e535f1
Timofey Stolbov <timofey@stolbov.info>
2014-12-10 17:17:29 +0500

    Fix b2a7fac

--- bin/bgset.sh
+++ bin/bgset.sh
@@ -5,10 +5,9 @@ CMD="feh --bg-scale"
 IMAGE_DIR=~/media/image/yandex
 
 if [ $# -eq 0 ]; then
-    if [ ! -d $IMAGE_DIR ]; then
-        exit 0
+    if [ -d $IMAGE_DIR ]; then
+        $CMD $(find "$IMAGE_DIR" -type f -print | sort -R | head -1)
     fi
-    $CMD $(find "$IMAGE_DIR" -type f -print | sort -R | head -1)
 else
     $CMD $1
 fi

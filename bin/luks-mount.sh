1ac8c60ff1df4dc2666804a85c65af01fd1fb83a
Timofey Stolbov <timofey@stolbov.info>
2015-07-19 13:13:59 +0500

    Use crypttab, Luke! :)

--- bin/luks-mount.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-UUID=${UUID-"43a4b92b-a084-4e38-a525-9bb853fe928e"}
-
-sudo cryptsetup luksOpen UUID=$UUID $UUID
-sudo mkdir -m 0775 /mnt/$UUID 2>/dev/null || true
-sudo mount /dev/mapper/$UUID /mnt/$UUID

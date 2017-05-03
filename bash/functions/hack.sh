1cff758016614481222041b0894af5bc215cd6fa
Timofey Stolbov <timofey@stolbov.info>
2017-05-03 14:25:16 +0500

    Autocomplete hack ()

--- bash/functions/hack.sh
+++ bash/functions/hack.sh
@@ -1,8 +1,20 @@
 hack () {
-    local hack_dir="${HOME}/stc/hack/${1}"
-    if ! [[ -d $hack_dir ]]; then
-        mkdir -p "${hack_dir}"
-        git -C "${hack_dir}" init
+    local root="${HOME}/src/hack/${1}"
+    if ! [[ -d ${root} ]]; then
+        mkdir -p "${root}"
+        git -C "${root}" init
     fi
-    cd "${hack_dir}" || exit
+    cd "${root}" || exit
 }
+
+_hack () {
+    local root="${HOME}/src/hack"
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=($(\
+        find "${root}" -type d -name .git -printf "%h\n"\
+        | cut -d/ -f6-\
+        | grep -i "${cur}"
+    ))
+}
+
+complete -F _hack hack

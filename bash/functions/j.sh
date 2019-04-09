bfe04e929677bae7733572a975bf8ce5c4ce3475
Timofey Stolbov <timofey@stolbov.info>
2019-04-09 15:06:40 +0500

    Add branch switcher function

--- bash/functions/j.sh
+++ bash/functions/j.sh
@@ -6,3 +6,9 @@ function jc () {
     cd $(fd --type d "${1:-.}" ~/src | fzf -1 --height=10)
     code .
 }
+
+function jb () {
+    local branches=$(git for-each-ref --sort=-committerdate refs/heads/ --format='%(refname:short)')
+    local branch=$(echo "${branches}" | fzf -1 --height=10)
+    git checkout -q ${branch}
+}
